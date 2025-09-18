b2fc7eb1cfbadd27d8ce76fa7e8009e4cafdfb41
MartinsSpru <martinssprugevics@gmail.com>
2025-09-18 16:46:57 +0300

    9opy9it8itooit

---
+++ 2025projects/resolution/script.js
@@ -0,0 +1,6 @@
+function named(){
+    let width = window.screen.width
+    let height = window.screen.height
+    document.getElementById('diplay').innerHTML = `${width} x ${height}`
+    document.getElementById('diplay').style.display = ''
+}
